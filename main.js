36009cd9b9bf446396c5daec1903e3c246823d6d
Steven <hellosteventran@gmail.com>
2020-03-20 23:04:02 +0000

    added JS functionality to the table

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var tableElement = document.querySelector("table");
+var headerElement = document.querySelector("header")
 var table = new GradeTable(tableElement);
-var app = new App(table);
+var app = new App(table, headerElement);
 app.start();
